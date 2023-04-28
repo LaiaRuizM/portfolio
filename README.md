7336df04ef1dc746c28d0ce7c5fcd9944f305512
LaiaRuizM <laia.ruizm@hotmail.com>
2023-04-28 20:41:38 +0200

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# portfolio
+<p align="center" style="margin-center:10%">
+<img src="./LaiaRuiz.png" alt="Laia" width="220" height="150"/>
+</p>
+
+# Portfolio | LaiaRuizM
+
+- Hi there people! 👋🏻
+- My name is Laia Ruiz Martínez and this is my **portfolio**.
---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+/* SECCIÓN DE IMPORT */
+// - De React
+// import { useEffect, useState } from "react";
+// import { Route, Routes } from "react-router-dom";
+// - Nuestros
+// - Sass
+import "../styles/App.scss";
+// - Imágenes
+/* SECCIÓN DEL COMPONENTE */
+function App() {
+  /* VARIABLES ESTADO (DATOS) */
+
+  /* EFECTOS (código cuando carga la página) */
+
+  /* FUNCIONES HANDLER */
+
+  /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+
+  /* HTML */
+  return <div className="App">{/* Aquí va el HTML */}</div>;
+}
+
+/* PROP-TYPES */
+
+/* EXPORT DEL COMPONENTE */
+export default App;
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h2>Este título aparece siempre</h2>
+
+//       <Routes>
+//         <Route
+//           path="/contacto"
+//           element={
+//             <h2>
+//               Este título solo aparece cuando la usuaria entra en la página de
+//               contacto
+//             </h2>
+//           }
+//         />
+//       </Routes>
+
+//       <nav>
+//         <ul>
+//           <li>
+//             <a href="/#/">Ir al inicio</a>
+//           </li>
+//           <li>
+//             <a href="/#/contacto">Ir a contacto</a>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default App;
